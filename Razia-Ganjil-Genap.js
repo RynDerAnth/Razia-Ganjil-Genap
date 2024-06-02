1273a3300d66a90e546f5fed69467af17d976147
Rayyn <166891366+RynDerAnth@users.noreply.github.com>
2024-06-02 14:13:22 +0700

    Update Razia-Ganjil-Genap.js

--- Razia-Ganjil-Genap.js
+++ Razia-Ganjil-Genap.js
@@ -1,9 +1,33 @@
 function kenaRazia(date, data) {
-
-  //code goes here
-
-}
+    let hasilRazia = [];
   
+    for (let i = 0; i < data.length; i++) {
+      let kendaraan = data[i];
+      if (kendaraan.type === "Mobil") {
+        let platNomor = kendaraan.plat.split(' ')[1];
+        let nomorBelakang = parseInt(platNomor[platNomor.length - 1]);
+        let isGanjil = date % 2 !== 0;
+        let tilang = 0;
+  
+        for (let j = 0; j < 
+             kendaraan.rute.length; j++) {
+          let rute = kendaraan.rute[j];
+          if (
+            (isGanjil && nomorBelakang % 2 === 0 && (rute === "Gajah Mada" || rute === "Hayam Wuruk" || rute === "Sisingamangaraja" || rute === "Panglima Polim" || rute === "Fatmawati" || rute === "Tomang Raya")) ||
+            (!isGanjil && nomorBelakang % 2 !== 0 && (rute === "Gajah Mada" || rute === "Hayam Wuruk" || rute === "Sisingamangaraja" || rute === "Panglima Polim" || rute === "Fatmawati" || rute === "Tomang Raya"))
+          ) {
+            tilang++;
+          }
+        }
+  
+        if (tilang > 0) {
+          hasilRazia.push({ name: kendaraan.name, tilang: tilang });
+        }
+      }
+    }
+  
+    return hasilRazia;
+}
 
 console.log(
 
@@ -86,36 +110,5 @@ console.log(
  ])
 
 );
-
-function kenaRazia(date, data) {
-    let hasilRazia = [];
-  
-    for (let i = 0; i < data.length; i++) {
-      let kendaraan = data[i];
-      if (kendaraan.type === "Mobil") {
-        let platNomor = kendaraan.plat.split(' ')[1];
-        let nomorBelakang = parseInt(platNomor[platNomor.length - 1]);
-        let isGanjil = date % 2 !== 0;
-        let tilang = 0;
-  
-        for (let j = 0; j < 
-             kendaraan.rute.length; j++) {
-          let rute = kendaraan.rute[j];
-          if (
-            (isGanjil && nomorBelakang % 2 === 0 && (rute === "Gajah Mada" || rute === "Hayam Wuruk" || rute === "Sisingamangaraja" || rute === "Panglima Polim" || rute === "Fatmawati" || rute === "Tomang Raya")) ||
-            (!isGanjil && nomorBelakang % 2 !== 0 && (rute === "Gajah Mada" || rute === "Hayam Wuruk" || rute === "Sisingamangaraja" || rute === "Panglima Polim" || rute === "Fatmawati" || rute === "Tomang Raya"))
-          ) {
-            tilang++;
-          }
-        }
-  
-        if (tilang > 0) {
-          hasilRazia.push({ name: kendaraan.name, tilang: tilang });
-        }
-      }
-    }
-  
-return hasilRazia;
-  }
 
 // [ { name: ‘Toni’, tilang: 1 }, { name: ‘Anna’, tilang: 3 } ]
